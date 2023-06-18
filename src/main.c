bfd941dcca0c9de932353b6ba3a8f7025fba74e3
SerjeiMikailov <felipemaders8@gmail.com>
2023-06-18 20:00:37 -0300

    g

--- src/main.c
+++ src/main.c
@@ -5,32 +5,64 @@
 #define MAX_WORDS 5000
 #define MAX_LENGTH 15
 char lines[MAX_LENGTH];
+char words1[MAX_WORDS][MAX_LENGTH];
+char words2[MAX_WORDS][MAX_LENGTH];
 
-void print_words(const char *filename)
+void print_unique_words(const char *filename1, const char *filename2, FILE *result)
 {
-    char filepath[100];
-    snprintf(filepath, sizeof(filepath), "src/assets/%s", filename);
+    char filepath1[100];
+    snprintf(filepath1, sizeof(filepath1), "src/assets/%s", filename1);
 
-    FILE *file = fopen(filepath, "r");
-    if (file == NULL)
+    FILE *file1 = fopen(filepath1, "r");
+    if (file1 == NULL)
     {
-        printf("Error to open the file: %s\n", filename);
+        printf("Error opening file: %s\n", filename1);
         return;
     }
 
-    char words[MAX_WORDS][MAX_LENGTH];
-    int count = 0;
-    
-    while (fscanf(file, "%s", words[count]) == 1 && count < MAX_WORDS)
-    {
-        count++;
-    }
-    fclose(file);
+    int count1 = 0;
 
-    for (int i = 0; i < count; i++)
+    while (fscanf(file1, "%s", words1[count1]) == 1 && count1 < MAX_WORDS)
     {
-        printf("%s\n", words[i]);
+        count1++;
     }
+    fclose(file1);
+
+    char filepath2[100];
+    snprintf(filepath2, sizeof(filepath2), "src/assets/%s", filename2);
+
+    FILE *file2 = fopen(filepath2, "r");
+    if (file2 == NULL)
+    {
+        printf("Error opening file: %s\n", filename2);
+        return;
+    }
+
+    int count2 = 0;
+    bool found;
+
+    while (fscanf(file2, "%s", words2[count2]) == 1 && count2 < MAX_WORDS)
+    {
+        found = false;
+
+        for (int i = 0; i < count1; i++)
+        {
+            if (strcmp(words2[count2], words1[i]) == 0)
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            printf("%s\n", words2[count2]);
+            fprintf(result, "{%s}\n", words2[count2]);
+        }
+
+        count2++;
+    }
+    fclose(file2);
 }
 
 int main()
@@ -38,7 +70,7 @@ int main()
     bool start = false;
     char start_res[5];
 
-    printf("Welcome to Between-Project, your text fixer\n");
+    printf("Welcome to text fixer\n");
     printf("Now, just type start\n");
     scanf("%s", start_res);
 
@@ -54,14 +86,17 @@ int main()
 
     FILE *result;
     result = fopen("src/assets/result.txt", "w");
+    if (result == NULL)
+    {
+        printf("Error creating the result file\n");
+        return 0;
+    }
 
     while (start == true)
     {
-        printf("Words from file 1:\n");
-        print_words("1.txt");
+        printf("Unique words from file 2:\n");
+        print_unique_words("1.txt", "2.txt", result);
 
-        printf("Words from file 2:\n");
-        print_words("2.txt");
         start = false;
     }
 
